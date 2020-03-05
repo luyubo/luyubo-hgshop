2969dc3acd74b0b4f6a90503cc9efa7cb316606f
luyubo <770286812@qq.com>
2020-03-05 20:24:51 +0800

    添加service层的mapper文件

--- hgshop-goods-manager/src/main/java/com/luyubo/hgshop/controller/SpecController.java
+++ hgshop-goods-manager/src/main/java/com/luyubo/hgshop/controller/SpecController.java
@@ -8,11 +8,14 @@
 */  
 package com.luyubo.hgshop.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.pagehelper.PageInfo;
 import com.luyubo.hgshop.pojo.Spec;
@@ -47,5 +50,85 @@ public class SpecController {
 		PageInfo<Spec> pageInfo = specService.list(name, page);
 		model.addAttribute("pageInfo", pageInfo);
 		return "spec/list";
+	}
+	
+	/**
+	 * 添加
+	 * @param request
+	 * @param page
+	 * @param name
+	 * @return
+	 */
+	@RequestMapping("add")
+	@ResponseBody
+	public String add(HttpServletRequest request,Spec spec) {
+		System.out.println("spec" + spec);
+		//System.out.println();
+		spec.getOptions().removeIf(x->{return x.getOptionName()==null;});
+		//System.out.println("spec 处理后：" + spec);
+		//调用服务
+		int add = specService.add(spec);
+		return add>0?"success":"false";
+	}
+	
+	@RequestMapping("update")
+	@ResponseBody
+	public String update(HttpServletRequest request,Spec spec) {
+		System.out.println("spec" + spec);
+		//System.out.println();
+		spec.getOptions().removeIf(x->{return x.getOptionName()==null;});
+		System.out.println("spec 处理后：" + spec);
+		//调用服务1
+		int add = specService.update(spec);  
+		//return add>0?"success":"false";
+		return "fail";
+	}
+	
+	
+	
+	/**
+	 * 用于修改数据时候的回显
+	 * @param request
+	 * @param id 规格id
+	 * @return
+	 */
+	@RequestMapping("getSpec")
+	@ResponseBody
+	public Spec getSpec(HttpServletRequest request, int id){
+		return specService.findById(id);
+		
+	}
+	
+	
+	/**
+	 * 删除规格
+	 * @param request
+	 * @param id  规格的id
+	 * @return
+	 */
+	@RequestMapping("delSpec")
+	@ResponseBody
+	public String delSpec(HttpServletRequest request,int id) {
+		//调用服务
+		int delNum = specService.delete(id);
+		return delNum>0?"success":"false";
+	}
+	
+	/**
+	 * 删除规格
+	 * @param request
+	 * @param id  规格的id
+	 * @return
+	 */
+	@RequestMapping("delSpecBatch")
+	@ResponseBody
+	public String delSpecBatch(HttpServletRequest request,@RequestParam(name="ids[]") int[] ids) {
+		System.out.println("要删除的额数据");
+		for (int i : ids) {
+			System.out.println(" i is " + i  );
+		}
+		//调用服务
+		int delNum = specService.deleteBatch(ids);
+		return delNum>0?"success":"false";
 	}
 }
